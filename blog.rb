6c8e5560e5ac1ca3276288baf0b9b17a293958ae
Ben Sheehan <sheehan1102@gmail.com>
2015-10-13 17:50:12 +0200

    Exercise complete

--- blog.rb
+++ blog.rb
@@ -1,5 +1,9 @@
+require 'colorize'
+require 'pry'
+
 class Blog
-	def initialize(posts = [])
+	def initialize(page_length = 3, posts = [])
+		@page_length = page_length
 		@posts = posts
 	end
 
@@ -7,10 +11,46 @@ class Blog
 		@posts << post
 	end
 
-	def publish_front_page
-		@posts.each do |post|
+	def paginate(posts, index)
+		current_page = (index / @page_length) + 1
+		pages = (posts.size / @page_length.to_f).ceil
+		pager = (1..pages).to_a
+
+		pager.each do |p|
+			if p == current_page
+				print p.to_s.colorize(:green) + ' '
+			else
+				print p.to_s + ' '
+			end
+		end
+		puts ' '
+		puts ' '
+
+		show_three_posts(posts, index, gets.chomp)
+	end
+
+	def show_three_posts(posts, index = 0, query = '')
+		if query == 'prev' && index > 0
+			index -= @page_length
+		elsif query == 'next' && index < (posts.size - @page_length)
+			index += @page_length
+		elsif query == 'quit'
+			return
+		end
+
+		posts[index..(index + (@page_length - 1))].each do |post|
 			post.post_thumbnail
 		end
+
+		paginate(posts, index)
+	end
+
+	def publish_front_page
+		ordered_posts = @posts.sort do |a, b|
+			b.date <=> a.date
+		end
+
+		show_three_posts(ordered_posts)
 	end
 end
 
@@ -21,6 +61,7 @@ class Post
 		@title = title
 		@text = text
 		@sponsored = sponsored
+		@date = Time.now
 	end
 
 	def post_thumbnail
@@ -35,9 +76,14 @@ class Post
 	end
 end
 
-blog 	= Blog.new
+blog = Blog.new
 blog.add_post(Post.new('My First Post', "I love my new blog!"))
 blog.add_post(Post.new('My Second Post', 'I still love my post!', true))
 blog.add_post(Post.new('My Third Post', 'I think this blog is pretty awesome'))
+blog.add_post(Post.new('My Fourth Post', 'I\'m still pretty stoked on this blog!'))
+blog.add_post(Post.new('My Fifth Post', 'Yeah, this blog is pretty cool.', true))
+blog.add_post(Post.new('My Sixth Post', 'I\'m sort of luke warm on this blog'))
+blog.add_post(Post.new('My Seventh Post', 'Um, not sure what I think of this blog'))
+blog.add_post(Post.new('My Eighth Post', 'Do I have to keep writing this blog?'))
 blog.publish_front_page
 
